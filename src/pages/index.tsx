5c36937b566c0a8b86d6336e0695e9ffe485f5b1
Igor <igor.korobyev@zoftify.com>
2021-12-01 02:51:58 +0300

    deploy on heroku

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const IndexPage: FC<IndexPageProps> = () => {
   const [isDisable, setIsDisable] = useState(true)
 
   useEffect(() => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('token');
     if (token) setIsDisable(false)
   })
 
